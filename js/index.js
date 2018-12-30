0653559349efcb50602bcc39e72148bd5344518b
Emmanuel Anzele <eanzele@gmail.com>
2018-12-30 04:02:24 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
-var ul = document.createElement('ul');
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li');
-  li.innerHTML = (i + 1).toString();
-  ul.appendChild(li);
-}
- 
-element.appendChild(ul);
+const element = document.createElement('div')
+element.innerHTML = 'Hello, DOM!'
+element.style.backgroundColor = 'pink'
+document.body.appendChild(element)
+element.style.textAlign = 'center'
+
+
+element.remove();
